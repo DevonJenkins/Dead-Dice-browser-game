63db52e37e5705dde00e872d7e25bd704e5ff95c
DVNJNKNS <devonmjenkins3@gmail.com>
2022-02-09 23:04:46 -0500

    layout js and add some function templates

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*-------------------------------- Constants --------------------------------*/
+const dice = [1,2,3,4,5,6]
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let currentPlayer = null
+let score = null 
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+function init(){}
+function render(){}
+function handleClick(){}
+function rollDice(){}
+function getWinner(){}
