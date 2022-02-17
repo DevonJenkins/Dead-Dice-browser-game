a8c1f79c162626a487342439ab9805ea996ce28b
DVNJNKNS <devonmjenkins3@gmail.com>
2022-02-17 17:16:13 -0500

    changing color scheme. Adding fuctionality to make the background change color based on turn and winner

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ rollBtn = document.querySelector('#roll-dice')
 statusMessage = document.querySelector('.status-message')
 p1ScoreDisplay = document.querySelector('#player-one-score')
 p2ScoreDisplay = document.querySelector('#player-two-score')
-
+htmlBody = document.querySelector('body')
 
 
 d1 = document.querySelector('#d1')
@@ -44,6 +44,8 @@ function init(){
   currentPlayer = 1
   playerOneScore = 0
   playerTwoScore = 0
+
+
 }
 
 
@@ -71,7 +73,7 @@ diceInPlay = [0,0,0,0,0]
     render()
   handleTurn()
   countScore()
-  pipRender()
+  
 
 }
 
@@ -86,7 +88,8 @@ function rollDice(min, max){
 function handleTurn(){
   if  ((discardedDice.length == 5)&&(currentPlayer == 1))
   {(currentPlayer = -1) && playerInit()}
-
+   if(currentPlayer == 1 ){htmlBody.style.background = 'rgb(238, 108, 77)'}
+  else if (currentPlayer == -1){htmlBody.style.background = 'rgb(0, 121, 140)' }
   
   endGame()
 }
@@ -134,12 +137,16 @@ function endGame(){
 
 
 function getWinner(){
+  
 
-  if(playerOneScore > playerTwoScore){statusMessage.textContent = 'player 1 wins'
-  }else if( playerTwoScore> playerOneScore){statusMessage.textContent = 'player 2 wins'
-  }else if (playerOneScore == playerTwoScore){statusMessage.textContent = 'its a tie sonny'}
+  if(playerOneScore > playerTwoScore){(statusMessage.textContent = 'player 1 wins') && (htmlBody.style.background = 'rgb(238, 108, 77)')
+  }else if( playerTwoScore> playerOneScore){(statusMessage.textContent = 'player 2 wins') && ( htmlBody.style.background = 'rgb(0, 121, 140)')
+  }else if (playerOneScore == playerTwoScore){statusMessage.textContent =( 'its a tie sonny')&&(htmlBody.style.background = 'rgb(48, 99, 142)')}
 
+
+  rollBtn.textContent = "reset"
   init()
+
   
 }
 
@@ -162,16 +169,30 @@ function render(){
   p2ScoreDisplay.textContent = 'p2 score: ' + playerTwoScore
 
   discardedDiceDisplay.textContent =  discardedDice.length
-
+  pipRender()
 }
 
 function pipRender(){
-if (d1.textContent == 1){
-  d1.style.color = red
-}
-   
-}
-  
+
+if (d1.textContent == 0){
+   (d1.textContent = '')
+} 
+if (d2.textContent == 0){
+  (d2.textContent = '')
+} 
+if (d3.textContent == 0){
+  (d3.textContent = '')
+} 
+if (d4.textContent == 0){
+  (d4.textContent = '')
+} 
+if (d5.textContent == 0){
+  (d5.textContent = '')
+} 
+
+
+
+ }
 
 function buttonChange(){
 //on gameover, change the roll button to a reset button 
